429846b2f28907b862bb86a175d5ae482e4e10f2
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-09 03:05:39 +0700

    Release v0.001

--- lib/Acme/MetaSyntactic/WordList.pm
+++ lib/Acme/MetaSyntactic/WordList.pm
@@ -7,34 +7,23 @@ use 5.010001;
 use strict 'subs', 'vars';
 use warnings;
 
-use parent qw(Acme::MetaSyntactic::List);
+use parent 'Acme::MetaSyntactic::List';
 
-sub new {
-    my $class = shift;
-    bless [], $class;
-}
-
-sub init {
+sub init_data {
     my ($self, $wl_module) = @_;
     (my $wl_module_pm = "$wl_module.pm") =~ s!::!/!g;
     require $wl_module_pm;
+    my $class = caller(0);
     my $data = {
+        # this is silly, really. converting data unnecessarily
         names => join(" ", $wl_module->new->all_words),
     };
-    $self->SUPER::init($data);
+    # sigh, can't do this because AM:List uses caller(0)
+    #$self->SUPER::init($data);
+    return $data;
 }
 
 1;
 # ABSTRACT: Get meta names from WordList::*
 
-=head1 SYNOPSIS
-
-In your F<lib/Acme/MetaSyntactic/yourtheme.pm>:
-
- use parent 'Acme::MetaSyntactic::WordList';
- __PACKAGE__->init
-
-=head1 DESCRIPTION
-
-This is a base class for C<Acme::MetaSyntactic::*> theme that wants to get the
-names from corresponding C<WordList::*> module.
+=for Pod::Coverage ^(.+)$
